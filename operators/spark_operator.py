e35052a0ddb4949e0356f49cbd03cd366f756bfe
Viktor Pecheniuk <vpec@ciklum.com>
2019-03-21 22:34:51 +0200

    + added watch logic for operator

--- operators/spark_operator.py
+++ operators/spark_operator.py
@@ -1,5 +1,6 @@
 from kubernetes import client as kube_client
 from kubernetes import config as kube_config
+from kubernetes import watch
 
 from airflow.exceptions import AirflowException
 from airflow.models import BaseOperator
@@ -21,23 +22,33 @@ class SparkJobOperator(BaseOperator):
         self.timeout = timeout
 
     def execute(self, context):
-        # create an instance of the API class
+        # TODO check later with prod kube
         config = kube_config.load_kube_config()
+        # create an instance of the API class
         api_instance = kube_client.CustomObjectsApi(kube_client.ApiClient(config))
         # params to create custom object
         group = 'sparkoperator.k8s.io'
         version = 'v1beta1'
         namespace = 'default'
         plural = 'sparkapplications'
+        spark_app_name = 'spark-pi'
         crd_body = yaml_2_json(self.crd_file)
+        w = watch.Watch()
+        api_params = [group, version, namespace, plural]
         try:
-            api_response = api_instance.create_namespaced_custom_object(group,
-                                                                        version,
-                                                                        namespace,
-                                                                        plural,
-                                                                        crd_body,
-                                                                        pretty=True)
+            api_instance.create_namespaced_custom_object(*api_params, crd_body, pretty=True)
         except AirflowException as e:
-            api_response = {}
             print("Exception when calling CustomObjectsApi -> create_namespaced_custom_object: %s\n" % e)
-        return api_response
+
+        for event in w.stream(api_instance.list_namespaced_custom_object, *api_params):
+            print(event.get('object', {}).get('status', {}).get('applicationState', {}).get('state'))
+            if event.get('object', {}).get('status', {}).get('applicationState', {}).get('state') == "COMPLETED":
+                break
+
+        try:
+            api_instance.delete_namespaced_custom_object(
+                group, version, namespace, plural, spark_app_name,
+                kube_client.V1DeleteOptions(), grace_period_seconds=10
+            )
+        except AirflowException as e:
+            print("Exception when calling CustomObjectsApi -> delete_namespaced_custom_object: %s\n" % e)
