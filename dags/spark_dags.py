510f8a4fd62d2ce408c38515df67352486d8dc1f
Viktor Pecheniuk <vpec@ciklum.com>
2019-03-25 21:14:37 +0200

    * refactoring

--- dags/spark_dags.py
+++ dags/spark_dags.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 from datetime import datetime, timedelta
 
@@ -11,7 +12,7 @@ from operators.spark_operator import SparkJobOperator
 default_args = {
     'owner': 'viktor',
     'depends_on_past': False,
-    'start_date': datetime(2019, 3, 1),
+    'start_date': datetime.today() - timedelta(days=1),
     'email': ['airflow@example.com'],
     'email_on_failure': False,
     'email_on_retry': False,
@@ -19,9 +20,11 @@ default_args = {
     'retry_delay': timedelta(minutes=5)
 }
 
-dag = DAG(dag_id='spark_dag',
-          default_args=default_args,
-          schedule_interval=timedelta(minutes=3))
+dag = DAG(
+    dag_id='spark_dag',
+    default_args=default_args,
+    schedule_interval=timedelta(minutes=60)
+)
 
 t1 = BashOperator(
     task_id='print_date',
@@ -32,7 +35,7 @@ t1 = BashOperator(
 test_job = SparkJobOperator(
     task_id='k8s_spark',
     namespace='default',
-    job_name='spark-pi',
+    job_name='spark-pi-{}'.format(int(time.time())),
     yml_file='{}/ci/kube/spark_prometheus.yml'.format(os.path.abspath('.')),
     timeout=300,
     dag=dag
--- operators/spark_operator.py
+++ operators/spark_operator.py
@@ -1,16 +1,19 @@
-from kubernetes.client import ApiClient, CustomObjectsApi, V1DeleteOptions
 import kubernetes.config as kube_config
+from kubernetes.client import ApiClient, CustomObjectsApi
 
 from kubernetes.watch import Watch
 
 from airflow.exceptions import AirflowException
 from airflow.models import BaseOperator
+
 from airflow.utils.decorators import apply_defaults
 
 from utils.helpers import yaml_2_json
 
 
 class SparkJobOperator(BaseOperator):
+
+    template_fields = ('namespace', 'job_name')
 
     @apply_defaults
     def __init__(self, namespace, job_name, yml_file, timeout, *args, **kwargs):
@@ -34,30 +37,18 @@ class SparkJobOperator(BaseOperator):
         version = 'v1beta1'
         plural = 'sparkapplications'
         namespace = self.namespace
-        crd_body = yaml_2_json(self.crd_file)
         spark_app_name = self.job_name
+        crd_body = yaml_2_json(self.crd_file)
+        crd_body['metadata']['name'] = spark_app_name
         w = Watch()
         params = [group, version, namespace, plural]
         try:
-            api_resp = api_instance.get_namespaced_custom_object(
-                group, version, namespace, plural, spark_app_name
-            )
-            print(api_resp.keys())
-            try:
-                if api_resp['metadata']['name'] == self.job_name:
-                    api_instance.delete_namespaced_custom_object(
-                        group, version, namespace, plural, spark_app_name,
-                        V1DeleteOptions(), grace_period_seconds=5
-                    )
-            except AirflowException as e:
-                print("Exception when calling CustomObjectsApi -> delete_namespaced_custom_object: %s\n" % e)
-
-            api_instance.create_namespaced_custom_object(*params, crd_body, pretty=True)
-
+            api_instance.create_namespaced_custom_object(*params, crd_body, pretty=False)
         except AirflowException as e:
             print("Exception when calling CustomObjectsApi -> create_namespaced_custom_object: %s\n" % e)
 
         for event in w.stream(api_instance.list_namespaced_custom_object, *params, timeout_seconds=self.timeout):
-            print(event.get('object', {}).get('status', {}).get('applicationState', {}).get('state'))
-            if event.get('object', {}).get('status', {}).get('applicationState', {}).get('state') == "COMPLETED":
+            job_name = event.get('object', {}).get('metadata', {}).get('name')
+            job_state = event.get('object', {}).get('status', {}).get('applicationState', {}).get('state')
+            if job_name == spark_app_name and job_state == "COMPLETED":
                 break
